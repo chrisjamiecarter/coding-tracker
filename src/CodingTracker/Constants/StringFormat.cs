7713d3b543cd2574303b59080b611ebb2d92716f
Chris Carter <chrisjamiecarter@gmail.com>
2024-07-05 18:35:14 +0100

    verified commit?

--- src/CodingTracker/Constants/StringFormat.cs
+++ src/CodingTracker/Constants/StringFormat.cs
@@ -1,5 +1,8 @@
 ﻿namespace CodingTracker.Constants;
 
+/// <summary>
+/// Any string format constants required by the Core project.
+/// </summary>
 public static class StringFormat
 {
     public static string DateTime = "yyyy-MM-dd HH:mm";
