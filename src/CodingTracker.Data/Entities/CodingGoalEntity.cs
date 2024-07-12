a498b4f7457cc47dd71cda95ae6e544728bcbf1e
Chris Carter <108958713+cjc-sweatbox@users.noreply.github.com>
2024-07-12 18:46:42 +0100

    Implemented user coding goals
    #5

---
+++ src/CodingTracker.Data/Entities/CodingGoalEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace CodingTracker.Data.Entities;
+
+public class CodingGoalEntity
+{
+    public CodingGoalEntity()
+    {
+        // Required for Dapper.
+    }
+
+    public int Id { get; init; }
+
+    public double WeeklyDurationInHours { get; init; }
+}
---
+++ src/CodingTracker/Models/CodingGoal.cs
@@ -0,0 +1,24 @@
+﻿using CodingTracker.Data.Entities;
+
+namespace CodingTracker.Models;
+
+/// <summary>
+/// Coding session data transformation object.
+/// </summary>
+public class CodingGoal
+{
+    public CodingGoal(CodingGoalEntity entity)
+    {
+        Id = entity.Id;
+        WeeklyDurationInHours = entity.WeeklyDurationInHours;
+    }
+
+    public CodingGoal(double weeklyDurationInHours)
+    {
+        WeeklyDurationInHours = weeklyDurationInHours;
+    }
+
+    public int Id { get; init; }
+
+    public double WeeklyDurationInHours { get; init; }
+}
